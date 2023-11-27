299b75c2f1637b6988307aabd7c8a2b0364d8c0f
Alfred M <alfred.mkg@proton.me>
2023-11-27 22:27:19 +0300

    Add CORS sonfiguration

--- config/application.rb
+++ config/application.rb
@@ -40,5 +40,12 @@ module HelloBackEndRails
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
+
+    config.middleware.insert_before 0, Rack::Cors do
+  allow do
+    origins 'http://localhost:3001'
+    resource '*', headers: :any, methods: [:get]
+  end
+end
   end
 end
